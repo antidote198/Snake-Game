f29686cb576f3962c4376ced531906bd37024211
antidote198 <segundojames@gmail.com>
2022-07-23 21:34:22 +0800

    added displayGameOver()

--- script.js
+++ script.js
@@ -145,5 +145,11 @@ function checkGameOver() {
 		}
 	}
 }
-function displayGameOver() {}
+function displayGameOver() {
+	context.font = "50px MV Boli";
+	context.fillStyle = "black";
+	context.textAlign = "center";
+	context.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
+	running = false;
+}
 function resetGame() {}
