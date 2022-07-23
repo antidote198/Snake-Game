d7d2fb406202ade1d3dda47025251045ddd6cd6b
antidote198 <segundojames@gmail.com>
2022-07-23 20:25:11 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const gameBoard = document.querySelector("#game-board");
+const context = gameBoard.getContext("2d");
+const scoreBoard = document.querySelector("#score-board");
+const resetBtn = document.querySelector("#reset-button");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "white";
+const snakeColor = "lightgreen";
+const snakeBorder = "black";
+const foodsnake = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+	{ x: unitSize * 4, y: 0 },
+	{ x: unitSize * 3, y: 0 },
+	{ x: unitSize * 2, y: 0 },
+	{ x: unitSize, y: 0 },
+	{ x: 0, y: 0 },
+];
